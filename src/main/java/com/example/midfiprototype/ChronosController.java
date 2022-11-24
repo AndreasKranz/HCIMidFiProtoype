51d608328c371178ffc8bdd337dd818664d468f2
AndreasKranz <andreas.kranz.22.09@gmail.com>
2022-11-24 15:52:03 +0900

    Added basic navbar

--- src/main/java/com/example/midfiprototype/ChronosController.java
+++ src/main/java/com/example/midfiprototype/ChronosController.java
@@ -1,52 +1,95 @@
 package com.example.midfiprototype;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Controller
 public class ChronosController {
 
+    @Autowired
+    ChronosService service;
+
+    ArrayList<Meeting> tempMeetings;
+
+    @PostMapping(value = "/createMeet", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createMeeting(@RequestBody MeetingDTO dto) {
+        tempMeetings.add(new Meeting(dto.getMeetingname(), dto.getParticipants(), dto.getMeetingTime(), LocalDateTime.now(), dto.getLocation(), dto.getDescription(), dto.getMeetingStatus()));
+
+        service.saveMeeting(tempMeetings);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Sucessfully saved new meeting!");
+    }
+
+    @PostMapping(value = "/editMeet", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> editMeeting(@RequestBody MeetingDTO dto) {
+
+        this.tempMeetings = service.editMeeting(tempMeetings, dto);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Sucessfully edited Meeting");
+    }
+
     @RequestMapping(value = "")
     public String viewIndex() {
+        if (tempMeetings.size() == 0) {
+            tempMeetings = service.loadMeetings();
+        }
         return "index";
     }
 
     @RequestMapping(value = "/index")
     public String viewIndex1() {
+        if (tempMeetings.size() == 0) {
+            tempMeetings = service.loadMeetings();
+        }
+
         return "index";
     }
 
     @RequestMapping(value = "/timeform")
     public String retrieveFindByTime() {
+
+        ArrayList<Meeting> meets = new ArrayList<>();
+        meets.add(new Meeting("testname", "leute,haha", LocalDateTime.now(), LocalDateTime.now(), "important adress", "long text", MeetingStatus.IN_VOTING));
+
+
+        service.saveMeeting(meets);
         return "findByTime";
     }
 
     @RequestMapping(value = "/locform")
-    public String openFindByLocation() {
+    public String retrieveFindByLocation() {
+        ArrayList<Meeting> meets1 = service.loadMeetings();
+
+        System.out.println(meets1.toString());
+
         return "findByLocation";
     }
 
-    @RequestMapping(value="/vote")
+    @RequestMapping(value = "/vote")
     public String openVoting() {
         return "voting";
     }
 
-    @PostMapping(value="/map")
-    public String retrieveMap(){
+    @PostMapping(value = "/map")
+    public String retrieveMap() {
         return "mapFragment";
     }
 
     @PostMapping(value = "/success")
-    public String retrieveSucess(){
+    public String retrieveSucess() {
         return "invSent";
     }
 
-    @GetMapping(value= "/searchResult")
-    public String retrieveSearchResult(){
+    @GetMapping(value = "/searchResult")
+    public String retrieveSearchResult() {
         return "keywordSearchResult";
     }
 
